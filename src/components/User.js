16d9cedf8b3466a795dd29cc78ddc1f0cc4a6aae
nihalcool123 <nihalmah123@gmail.com>
2020-11-11 13:20:03 +0530

    theme

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import SkeletonProfile from "../skeletons/SkeletonProfile";
 
 const User = () => {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+
+      const data = await res.json();
+      setProfile(data);
+    }, 2000);
+  });
+
   return (
     <div classname="user">
-      <h2>User</h2>
+      <h2>User Details</h2>
+      {profile && (
+        <div className="profile">
+          <h3> {profile.username} </h3>
+          <p>{profile.email}</p>
+          <a href={profile.website}>{profile.website}</a>
+        </div>
+      )}
+
+      {!profile && <SkeletonProfile theme="dark" />}
     </div>
   );
 };
